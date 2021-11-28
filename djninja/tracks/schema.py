cd128b1fe7368c07b4bf13061cbbdc3f3d410c4c
jwun95 <jwun95@naver.com>
2021-11-29 01:31:59 +0900

    ninja file, modelschema

--- djninja/tracks/schema.py
+++ djninja/tracks/schema.py
@@ -1,11 +1,20 @@
 from datetime import datetime
-from ninja import Schema
+from ninja import Schema, ModelSchema
+from ninja.orm import create_schema
+from .models import Track
 
-class TrackSchema(Schema):
+TrackSchema = create_schema(Track, fields=['title', 'last_play', 'artist', 'duration'])
+
+""" class TrackSchema(Schema):
     title: str
     artist: str
     duration: float
-    last_play: datetime
+    last_play: datetime """
+
+""" class TrackSchema(ModelSchema):
+    class Config:
+        model = Track
+        model_fields = ['title', 'last_play', 'artist', 'duration'] """
 
 class NotFoundSchema(Schema):
     message: str
